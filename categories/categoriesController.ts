9bb078e1c7f2557e84d193bfd61abd0cdf9e8535
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-18 20:49:33 -0300

    $$ Arrumando bugs bootstrap

--- categories/categoriesController.ts
+++ categories/categoriesController.ts
@@ -1,14 +1,9 @@
 const expressCategories = require("express");
 const router = expressCategories.Router();
 
-
-router.get("/categories", (req:any, res:any) =>{
-    res.send("Rota de categorias")
+router.get("/admin/categories/new", (req: any, res :any)=>{
+    res.render("admin/categories/new")
 });
-
-router.get("/admin/categories/new", (req:any, res: any)=>{
-    res.send("Rota para criar nova categorria")
-})
 
 
 module.exports = router
