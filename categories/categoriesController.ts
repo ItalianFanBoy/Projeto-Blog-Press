602191744eea7e6ae21078a31caf9e07ccace18b
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-18 22:13:45 -0300

    arrumando primeiro POST

--- categories/categoriesController.ts
+++ categories/categoriesController.ts
@@ -1,9 +1,27 @@
 const expressCategories = require("express");
 const router = expressCategories.Router();
+const CategorySql = require("./category");
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req: any, res :any)=>{
-    res.render("admin/categories/new")
+    res.render("admin/categories/new");
 });
 
+
+router.post("/categories/save", (req:any, res:any)=>{
+    var title = req.body.title;
+    if(title != undefined){
+
+        CategorySql.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect("/")
+        })
+
+    }else{
+        res.redirect("/admin/categories/new");
+    }
+});
 
 module.exports = router
