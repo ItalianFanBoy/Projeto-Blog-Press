02028a0a6b96c624740856720e33be671a2dea06
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-20 20:55:49 -0300

    Finalizando função de deletar artigos
    função de deletar artigos finalizada

--- articles/articlesController.ts
+++ articles/articlesController.ts
@@ -5,14 +5,19 @@ const ArticlesforMysql =  require("./articles")
 const slugfyforArticles =  require("slugify")
 
 routerArticles.get("/articles", (req:any, res:any) =>{
-    res.send("Rota de artigos")
+    ArticlesforMysql.findAll({
+        include: [{model: CategoryforArticles}]
+    }).then((article: any)=>{
+        res.render("admin/articles/index",{articles: article })
+    })
 });
 
 routerArticles.get("/admin/articles/new", (req:any, res: any)=>{
     CategoryforArticles.fildAll().then((categories:any) =>{
-        res.render("admin/articles/new", (categories: categories)}
-    })
+        res.render("admin/articles/new", {categories: categories})
    
+        })
+
 })
 
 
@@ -27,8 +32,32 @@ routerArticles.post("/articles/save", (req: any, res:any)=>{
         slug: slugfyforArticles(title),
         body: body,
         categoryId: category,
+    }).then(()=>{
+        res.redirect("/")
     })
 })
+
+router.post("/articles/delete", (req:any, res:any)=>{
+    var id:any = req.body.id;
+    if(id != undefined){
+       if(!isNaN(id)){
+
+        CategorySql.destroy({
+            where:{
+                id: id,
+            }
+        }).then(()=>{
+            res.redirect("/admin/categories")
+        })
+
+    }else{ // nao for or numero
+        res.redirect("/admin/articles");
+    }
+
+    }else{ // null
+        res.redirect("/admin/articles");
+    }
+});
 
 
 module.exports = routerArticles
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const connection = new Sequelize('guiapress', 'root', 'Belluno2005',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone:"-03-00"
 });
 
 
