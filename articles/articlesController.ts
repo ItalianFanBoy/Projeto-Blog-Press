544cca6aedf22dcc02a7622719274d80a0010be9
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-06-20 20:37:43 -0300

    $$ arrumando bugs $$

--- articles/articlesController.ts
+++ articles/articlesController.ts
@@ -1,13 +1,33 @@
 const expressarticles = require("express");
 const routerArticles = expressarticles.Router();
-
+const CategoryforArticles = require("../categories/category") 
+const ArticlesforMysql =  require("./articles")
+const slugfyforArticles =  require("slugify")
 
 routerArticles.get("/articles", (req:any, res:any) =>{
     res.send("Rota de artigos")
 });
 
 routerArticles.get("/admin/articles/new", (req:any, res: any)=>{
-    res.render("admin/articles/new")
+    CategoryforArticles.fildAll().then((categories:any) =>{
+        res.render("admin/articles/new", (categories: categories)}
+    })
+   
+})
+
+
+routerArticles.post("/articles/save", (req: any, res:any)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+
+    ArticlesforMysql.create({
+        title: title,
+        slug: slugfyforArticles(title),
+        body: body,
+        categoryId: category,
+    })
 })
 
 
